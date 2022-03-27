7092d0ffa9021980506c590924cb1dee82776354
Oussema Trabelsi <79041057+aws3ma@users.noreply.github.com>
2022-03-27 13:31:23 +0100

    delete files from old project

--- home/views.py
+++ home/views.py
@@ -1,15 +1,7 @@
-from ast import Try
-from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .Aiscript import Taquin
-from . import Aiscript
 from .Environment import Environment
-from .Taquin import Taquin
-
-
-
 def index(request):
 
     response = json.dumps([{}])
@@ -23,12 +15,6 @@ def resolve(request):
         data = json.loads(request.body)
         n = data["n"]
         br = data["data"]
-        # for i in range(n*n):
-        #     br.append(int(data["data"]["a"+str(i)]))
-        # Aiscript.END=Aiscript.makegoal(br)
-        # if(n==4):
-        #     Aiscript.END=[[1,2,3,4], [ 5,6,7,8], [9,10,11,12],[13,14,15,0]]
-        # b = Aiscript.main(br,n)
         a = Environment(n,br)
         res = a.expand(a.idaStar)
        
